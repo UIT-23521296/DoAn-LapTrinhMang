68fc466ccbea6a1c5197de91e81cacdbc4920948
UIT-23521815 <23521815@gm.uit.edu.vn>
2025-05-13 07:10:20 +0700

    Bàn cờ hoàn chỉnh

---
+++ MonopolyWinForms/MonopolyWinForms/Play_area/Draw_playarea.cs
@@ -0,0 +1,24 @@
+﻿using MonopolyWinForms.GameLogic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonopolyWinForms.Play_area
+{
+    public partial class Draw_playarea: Form
+    {
+        private Board Board;
+        public Draw_playarea()
+        {
+            InitializeComponent();
+            Board = new Board("D:\\UIT\\lap trinh mang\\doan\\DoAn-LapTrinhMang\\MonopolyWinForms\\MonopolyWinForms\\Play_area\\Tiles.txt");  // Đảm bảo file Tiles.txt nằm trong thư mục đúng
+            Board.GenerateBoard(this, Board.TileList);  // Hiển thị bàn cờ lên form
+        }
+    }
+}
--- MonopolyWinForms/MonopolyWinForms/Program.cs
+++ MonopolyWinForms/MonopolyWinForms/Program.cs
@@ -1,5 +1,6 @@
 using MonopolyWinForms.Login_Signup;
 using MonopolyWinForms.Home;
+using MonopolyWinForms.Play_area;
 namespace MonopolyWinForms
 {
     internal static class Program
@@ -13,7 +14,8 @@ namespace MonopolyWinForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Main_login_signup());
+            //Application.Run(new Main_login_signup());
+            Application.Run(new Draw_playarea());
         }
     }
 }
